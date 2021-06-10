87147a9a775b0c36f5ccd56c7e97b23cd3a8657d
Daniela Guzman <danyela822@hotmail.com>
2021-06-10 02:27:00 -0500

    Integracion de los metodos de algunos de los botones de la vista

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : Reference
 {
@@ -10,20 +11,47 @@ public class GameController : Reference
      */
     public void OnClickButtons(string name_button)
     {
-        //EL boton pause abre el canvas del menu de pause
+        //El boton pause abre el canvas del menu de pause
         if(name_button == "Button Pause")
         {
-
+            App.generalView.gameView.PauseCanvas.enabled = true;
         }
         //El boton help abre el tutorial del juego
         if (name_button == "Button Help")
         {
-
+            App.generalView.gameView.TutorialCanvas.enabled = true;
         }
-        //El boton solucion abre un canvas para ir a los minijuegos
+        //El boton solution abre un canvas para ir a los minijuegos
         if (name_button == "Button Solution")
         {
+            App.generalView.gameView.SolutionCanvas.enabled = true;
+        }
 
+        ////////////////////////////////// Botones del menu de pausa, tutorial y solucion ////////////////////////////////
+
+        //El boton back regresa a la partida
+        if (name_button == "Button Back")
+        {
+            if (App.generalView.gameView.PauseCanvas.enabled == true)
+            {
+                App.generalView.gameView.PauseCanvas.enabled = false;
+            }
+
+            if (App.generalView.gameView.SolutionCanvas.enabled == true)
+            {
+                App.generalView.gameView.SolutionCanvas.enabled = false;
+            }
+
+            if (App.generalView.gameView.TutorialCanvas.enabled == true)
+            {
+                App.generalView.gameView.TutorialCanvas.enabled = false;
+            }
+        }
+        //El boton back regresa al menu principal
+        if (name_button == "Button Menu")
+        {
+            //Falta la escena del menu principal
+            //SceneManager.LoadScene("");
         }
     }
 }
--- Assets/Scripts/GameView.cs
+++ Assets/Scripts/GameView.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class GameView : Reference
 {
+    //Declaracion de los canvas que contiene la vista del juego
+    public Canvas GameCanvas, PauseCanvas, SolutionCanvas, TutorialCanvas;
+
+    private void Awake()
+    {
+        GameCanvas = GameObject.Find("Game Canvas").GetComponent<Canvas>();
+        PauseCanvas = GameObject.Find("Pause Canvas").GetComponent<Canvas>();
+        SolutionCanvas = GameObject.Find("Solution Canvas").GetComponent<Canvas>();
+        TutorialCanvas = GameObject.Find("Tutorial Canvas").GetComponent<Canvas>();
+    }
+
     /*
      * Metodo que captura el nombre del boton el cual se esta pulsando
      */
