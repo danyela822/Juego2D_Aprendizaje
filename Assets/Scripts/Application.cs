bdc2837c5eed12e802bb147034de2eacd9ec97c5
Kelly Cardenas <kacsalazar.0827@gmail.com>
2021-06-12 16:02:39 -0500

    se agrega los scripts de vista y controlador

---
+++ Assets/Scripts/Application.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Application : MonoBehaviour
+{
+    public GeneralView generalView;
+    public GeneralController generalController;
+}
---
+++ Assets/Scripts/Reference.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reference : MonoBehaviour
+{
+    public Application App
+    {
+        get
+        {
+            return GameObject.FindObjectOfType<Application>();
+        }
+    }
+
+}
