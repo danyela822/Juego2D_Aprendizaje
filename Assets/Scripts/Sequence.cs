aeb38943097e9dbac33c062d70f3896b448e2bb4
Kelly Cardenas <kacsalazar.0827@gmail.com>
2021-07-09 20:52:21 -0500

    creacion del codigo que permite seleccionar el objeto y verificar la respuesta correcta

--- Assets/Scripts/Sequence.cs
+++ Assets/Scripts/Sequence.cs
@@ -6,23 +6,42 @@ public class Sequence : MonoBehaviour
 {
     //id del la imagen de la secuencia
     public string id;
-
     //imagen con que voy a comparar si escoge bien o no 
-    private Sequence sequenceToCompare = null;
+    private static Sequence sequenceToCompare = null;
 
     //varibale que me indica si la imagen es de enunciado o para comparar
     //false es enunciado
-    private bool type = false;
+    public bool type;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //metodo que permite comparar el objeto seleccionado con la respuesta correcta
+    private void SelectSequence(){
+
+        sequenceToCompare = gameObject.GetComponent<Sequence>();
+        string compare = SequenceController.sharedInstance.correctAnswer.GetComponent<Sequence>().id;
+
+        if (compare == this.id)
+        {
+            Debug.Log("Esta carajada dio a la primera");
+        }else
+        {
+            Debug.Log("No dio a la primera :(");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //metodo que permite seleccionar el objeto
+    private void OnMouseDown(){
+
+        //verifica si el objeto puede o no ser seleccionado
+        if(type == false){
+
+            return; 
+
+        }else{
+
+            SelectSequence();
+
+        }
+
     }
+
 }
